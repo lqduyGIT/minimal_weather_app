a9675ec66a4112872998557c8e13ead0a62cab40
DUYLUONG <lqduy242@gmail.com>
2024-11-09 19:35:59 +0700

    change 1

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:minimal_weather_app/services/weather_service.dart';
 
@@ -72,21 +73,46 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //locaiton animation
+            Lottie.asset(
+              'assets/location.json',
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover, //Điều chỉnh animation vừa với vùng chứa
+            ),
             //city name
-            Text(_weather?.cityName ?? "loading city.."),
-
+            Text(
+              _weather?.cityName ?? "loading city..",
+              style: GoogleFonts.poppins(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             //animation
             Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
 
             //temperatur
-            Text('${_weather?.temperature.round()}*C'),
+            Text(
+              '${_weather?.temperature.round()}*C',
+              style: GoogleFonts.poppins(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
 
             //weather condition
-            Text(_weather?.mainCondition ?? "")
+            Text(
+              _weather?.mainCondition ?? "",
+              style: GoogleFonts.poppins(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
           ],
         ),
       ),
